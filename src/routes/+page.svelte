78ac18c3b9649c9801793d17027b6310f567e522
Singularity Software <124126097+singularitysoft@users.noreply.github.com>
2023-02-17 09:13:00 -0500

    added initial html markup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="main">
+  <header>
+    <div class="container">
+      <h1>e-Publishing Platform</h1>
+      <div class="sign-in">
+        <a href="#">Sign In</a>
+        <a href="#">Log Out</a>
+      </div>
+    </div>
+  </header>
+
+  <section class="auth-panel">
+    <div class="container">
+      <h2>Sign in with:</h2>
+      <div class="auth-buttons">
+        <button class="auth-google">Sign in with Google</button>
+        <button class="auth-facebook">Sign in with Facebook</button>
+        <button class="auth-github">Sign in with Github</button>
+        <button class="auth-twitter">Sign in with Twitter</button>
+      </div>
+    </div>
+  </section>
+
+  <section class="main-content">
+    <div class="container">
+      <!-- Main content goes here -->
+    </div>
+  </section>
+
+  <footer>
+    <div class="container">
+      <p>&copy; 2023 e-Publishing Platform</p>
+    </div>
+  </footer>
+</div>
